293e5b19a46448f570cda4caa7a10d73006f7af5
Dawid <dawid1234x@wp.pl>
2023-10-22 13:48:34 +0200

    2 zadanie done

--- 2 Zadanie/.idea/sonarlint/issuestore/index.pb
+++ 2 Zadanie/.idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,7 @@ e
 5app/src/main/java/com/example/a2zadanie/Question.java,9\c\9c045245e4bac98e23a55314ee1120994656b314
 i
 9app/src/main/java/com/example/a2zadanie/MainActivity.java,2\9\29a371ccb58fc160b29f2f5c3b485604ac6e5ec6
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+k
+;app/src/main/java/com/example/a2zadanie/PromptActivity.java,a\9\a9f40bd680c52b5d2dde59c8aa32b108b155fd92
--- 2 Zadanie/app/src/main/java/com/example/a2zadanie/MainActivity.java
+++ 2 Zadanie/app/src/main/java/com/example/a2zadanie/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.a2zadanie;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,9 +13,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String KEY_EXTRA_ANSWER = "com.example.a2zadanie.CorrectAnswer";
+    private static final String KEY_CURRENT_POINTS = "points";
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    private static final int REQUEST_CODE_PROMPT = 0;
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
+    private Button answerButton;
     private TextView questionTextView;
     private Question[] questions = new Question[]{
             new Question(R.string.q_activity, true),
@@ -22,24 +30,44 @@ public class MainActivity extends AppCompatActivity {
             new Question(R.string.q_version, false)
     };
     private int currentIndex = 0;
+    private int points = 0;
+    private boolean answerWasShown;
     private void checkAnswerCorrectness(boolean userAnswer){
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
-        if(userAnswer == correctAnswer){
-            resultMessageId = R.string.correct_answer;
+        if(answerWasShown){
+            resultMessageId = R.string.answer_was_shown;
         }else{
-            resultMessageId = R.string.incorrect_answer;
+            if(userAnswer == correctAnswer){
+                resultMessageId = R.string.correct_answer;
+                points++;
+            }else{
+                resultMessageId = R.string.incorrect_answer;
+            }
         }
         Toast.makeText(this,resultMessageId,Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("call","onCreate");
         setContentView(R.layout.activity_main);
+        if (savedInstanceState !=null)
+        {
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
         questionTextView = findViewById(R.id.question_text_view);
+        answerButton = findViewById(R.id.answer_Button);
+
+        answerButton.setOnClickListener((v) -> {
+            Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+            boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+            intent.putExtra(KEY_EXTRA_ANSWER, correctAnswer);
+            startActivityForResult(intent, REQUEST_CODE_PROMPT);
+        });
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,8 +83,16 @@ public class MainActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentIndex = (currentIndex+1)%questions.length;
-                setNextQuestion();
+                answerWasShown = false;
+                if(currentIndex==4)
+                {
+                    questionTextView.setText("Zdobyłeś "+points+" pkt");
+                }
+                else{
+                    currentIndex = (currentIndex+1)%questions.length;
+                    setNextQuestion();
+                }
+
             }
         });
         setNextQuestion();
@@ -64,4 +100,55 @@ public class MainActivity extends AppCompatActivity {
     private void setNextQuestion(){
         questionTextView.setText(questions[currentIndex].getQuestionId());
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("call", "onStart");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("call","onPause");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("call","OnResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("call","OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("call","OnDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        Log.d("call","Wywołana została metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+        outState.putInt(KEY_CURRENT_POINTS, points);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != RESULT_OK) {
+            return ;
+        }
+        if(requestCode == REQUEST_CODE_PROMPT){
+            if(data == null) {
+                return;
+            }
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
+    }
+
 }
